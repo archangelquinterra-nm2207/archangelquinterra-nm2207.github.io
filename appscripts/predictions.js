711212a637b436727c2bd7de3910442005c900fa
archangelquinterra-nm2207 <87387526+archangelquinterra-nm2207@users.noreply.github.com>
2023-03-23 23:28:21 +0800

    Update predictions.js

--- appscripts/predictions.js
+++ appscripts/predictions.js
@@ -1,23 +1,29 @@
 
-
+//declare function to be executed once the button is clicked
 function recapPredictions() {
 
-    	var guestName = document.getElementById("name").value;
+    //obtain guest's name from the form
+        var guestName = document.getElementById("name").value;
+    //create a dialog box greeting using the guest's name obtained from the form
       var guestGreeting = "Thanks for submitting your recap, " + guestName + "! Please save the following page to keep your predictions! :)";
    		window.alert(guestGreeting);
+    //create a sentence using the guest's name on the HTML document, last sentence in the HTML document
       document.getElementById("greeting").innerHTML = "May your predictions come true, " + guestName + "!"
       
     //console.log(document.getElementById("name").value);
 
+    //create a sentence using the guest's name on the HTML document
     document.getElementById("recapTitle").innerHTML = guestName + "'s Formula 1 2023 Season Predictions"
 
     //const element = document.getElementById("constructorsChampionImage");
 
-    var driversChampion = document.getElementById("drivers").value;
-    document.getElementById("driversChampionResult").innerHTML = "I predict that " + driversChampion + " will win the 2023 Drivers' Champion!"   
-    console.log(driversChampion);
-      var driversChampionImage = document.getElementById("driversChampionImage");
-    if (driversChampion == "Max Verstappen") {
+    //Drivers' Champion predictions
+    var driversChampion = document.getElementById("drivers").value; //get the user's prediction for driver's champion
+    document.getElementById("driversChampionResult").innerHTML = "I predict that " + driversChampion + " will win the 2023 Drivers' Champion!" //use the predictions obtained to create a statement  
+    console.log(driversChampion); //an internal test to see whether the users' drivers' champion choice is correctly extracted
+      var driversChampionImage = document.getElementById("driversChampionImage"); //places the image of the user's drivers' champion choice on the corresponding container
+   //if else statement that will show an image according to the user's choice
+    if (driversChampion == "Max Verstappen") { 
       driversChampionImage.setAttribute("src", "resources/MaxVerstappen.jpg");
     } else if (driversChampion == "Charles Leclerc") {
       driversChampionImage.setAttribute("src", "resources/CharlesLeclerc.jpg");
@@ -36,13 +42,16 @@ function recapPredictions() {
     } else {
       driversChampionImage.setAttribute("src", "resources/Others.jpg");
     }
-    driversChampionImage.setAttribute("width", "500px");
+    //standardizing the image's width and height
+    driversChampionImage.setAttribute("width", "500px"); 
     driversChampionImage.setAttribute("height", "300px");
  
-    var constructorsChampion = document.getElementById("constructors").value;
-    document.getElementById("constructorsChampionResult").innerHTML = constructorsChampion + " will win the 2023 Constructors' Champion!"
-    console.log(constructorsChampion);
-      var constructorsChampionImage = document.getElementById("constructorsChampionImage");
+    //Constructors' Champion predictions
+    var constructorsChampion = document.getElementById("constructors").value; //get the user's prediction for constructors's champion
+    document.getElementById("constructorsChampionResult").innerHTML = constructorsChampion + " will win the 2023 Constructors' Champion!" //use the predictions obtained to create a statement 
+    console.log(constructorsChampion); //an internal test to see whether the users' constructors' champion choice is correctly extracted
+      var constructorsChampionImage = document.getElementById("constructorsChampionImage"); //places the image of the user's constructors' champion choice on the corresponding container
+    //if else statement that will show an image according to the user's choice
     if (constructorsChampion == "Red Bull") {
       constructorsChampionImage.setAttribute("src", "resources/RedBull.jpg");
     } else if (constructorsChampion == "Ferrari") {
@@ -64,13 +73,16 @@ function recapPredictions() {
     } else {
       constructorsChampionImage.setAttribute("src", "resources/Williams.jpg");
     }
+    //standardizing the image's width and height
     constructorsChampionImage.setAttribute("width", "500px");
     constructorsChampionImage.setAttribute("height", "300px");
 
-    var teamSurprise = document.getElementById("surprise").value;
-    document.getElementById("teamSurpriseResult").innerHTML = teamSurprise + " will be a pleasant surprise this season!"
-    console.log(teamSurprise);
-      var teamSurpriseImage = document.getElementById("teamSurpriseImage");
+    //Pleasant surprise predictions
+    var teamSurprise = document.getElementById("surprise").value; //get the user's prediction for pleasant surprise
+    document.getElementById("teamSurpriseResult").innerHTML = teamSurprise + " will be a pleasant surprise this season!" //use the predictions obtained to create a statement 
+    console.log(teamSurprise); //an internal test to see whether the users' team choice is correctly extracted
+      var teamSurpriseImage = document.getElementById("teamSurpriseImage"); //places the image of the user's team choice on the corresponding container
+    //if else statement that will show an image according to the user's choice
     if (teamSurprise == "Red Bull") {
       teamSurpriseImage.setAttribute("src", "resources/RedBull.jpg");
     } else if (teamSurprise == "Ferrari") {
@@ -92,13 +104,16 @@ function recapPredictions() {
     } else {
       teamSurpriseImage.setAttribute("src", "resources/Williams.jpg");
     }
+    //standardizing the image's width and height
     teamSurpriseImage.setAttribute("width", "500px");
     teamSurpriseImage.setAttribute("height", "300px");
 
-  	var rookieOfTheYear = document.getElementById("rookieOfTheYear").value;
-    document.getElementById("rookieResult").innerHTML = "Rookie of the year 2023 award will go to: " + rookieOfTheYear + "!";
-    console.log(rookieOfTheYear);
-      var rookieImage = document.getElementById("rookieImage");
+    //Rookie Of The Year choice
+  	var rookieOfTheYear = document.getElementById("rookieOfTheYear").value; //get the user's prediction for rookie of the year
+    document.getElementById("rookieResult").innerHTML = "Rookie of the year 2023 award will go to: " + rookieOfTheYear + "!"; //use the predictions obtained to create a statement 
+    console.log(rookieOfTheYear); //an internal test to see whether the users' rookie choice is correctly extracted
+      var rookieImage = document.getElementById("rookieImage"); //places the image of the user's rookie choice on the corresponding container
+    //if else statement that will show an image according to the user's choice
     if (rookieOfTheYear == "Nyck de Vries") {
       rookieImage.setAttribute ("src", "resources/NyckdeVries.jpg");
     } else if (rookieOfTheYear == "Oscar Piastri") {
@@ -106,10 +121,12 @@ function recapPredictions() {
     } else {
       rookieImage.setAttribute ("src", "resources/LoganSargeant.jpg");
     }
+    //standardizing the image's width and height
     rookieImage.setAttribute("width", "500px");
-    rookieImages.setAttribute("height", "300px");
+    rookieImage.setAttribute("height", "300px");
 }
 
+//calls the function above when the recapButton is clicked
 document.getElementById("recapButton").onclick = function() {
   recapPredictions();
 }
